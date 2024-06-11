75eacb3de972be5d0c6acdfb78ad4540d9fc671d
Lily <egzozu.be.bas@gmail.com>
2024-06-12 01:30:37 +0300

    Update Lua.hx

--- source/llua/Lua.hx
+++ source/llua/Lua.hx
@@ -38,28 +38,24 @@ extern class Lua {
 	static function pushnumber(L:cpp.RawPointer<Lua_State>, n:Lua_Number):Void;
 
 	@:native('lua_pushinteger')
-	static function pushinteger(L:cpp.RawPointer<Lua_State>, n:Lua_Integer):Void;
+	static function pushinteger(L:cpp.RawPointer<Lua_State>, n:Int):Void;
 
 	@:native('lua_pushlstring')
-	static function pushlstring(L:cpp.RawPointer<Lua_State>, s:cpp.ConstCharStar, len:cpp.SizeT):Void;
+	static function pushlstring(L:cpp.RawPointer<Lua_State>, s:String, len:Int):Void;
 
 	@:native('lua_pushstring')
-	static function pushstring(L:cpp.RawPointer<Lua_State>, s:cpp.ConstCharStar):Void;
-
-	@:native('lua_pushvfstring')
-	static function pushvfstring(L:cpp.RawPointer<Lua_State>, s:cpp.ConstCharStar, argp:cpp.VarList):Void;
-
-	@:native('lua_pushfstring')
-	static function pushfstring(L:cpp.RawPointer<Lua_State>, fmt:cpp.ConstCharStar, args:cpp.Rest<cpp.VarArg>):cpp.ConstCharStar;
+	static function pushstring(L:cpp.RawPointer<Lua_State>, s:String):Void;
 
 	@:native('lua_pushcclosure')
 	static function pushcclosure(L:cpp.RawPointer<Lua_State>, fn:Lua_CFunction, n:Int):Void;
 
+	@:noCompletion
 	@:native('lua_pushboolean')
-	static function pushboolean(L:cpp.RawPointer<Lua_State>, b:Int):Void;
+	static function _pushboolean(L:cpp.RawPointer<Lua_State>, b:Int):Void;
 
-	@:native('lua_pushlightuserdata')
-	static function pushlightuserdata(L:cpp.RawPointer<Lua_State>, p:cpp.RawPointer<cpp.Void>):Void;
+	static inline function pushboolean(L:cpp.RawPointer<Lua_State>, b:Bool):Void {
+		_pushboolean(L, b == true ? 1 : 0);
+	}
 
 	@:native('lua_pushthread')
 	static function pushthread(L:cpp.RawPointer<Lua_State>):Int;
@@ -71,16 +67,16 @@ extern class Lua {
 	static function type(L:cpp.RawPointer<Lua_State>, idx:Int):Int;
 
 	@:native('lua_typename')
-	static function typename(L:cpp.RawPointer<Lua_State>, tp:Int):cpp.ConstCharStar;
+	static function typename(L:cpp.RawPointer<Lua_State>, tp:Int):String;
 
 	@:native('lua_setglobal')
-	static function setglobal(L:cpp.RawPointer<Lua_State>, s:cpp.ConstCharStar):Int;
+	static function setglobal(L:cpp.RawPointer<Lua_State>, s:String):Int;
 
 	@:native('lua_getglobal')
-	static function getglobal(L:cpp.RawPointer<Lua_State>, s:cpp.ConstCharStar):Int;
+	static function getglobal(L:cpp.RawPointer<Lua_State>, s:String):Int;
 
 	@:native('lua_tostring')
-	static function tostring(L:cpp.RawPointer<Lua_State>, i:Int):cpp.ConstCharStar;
+	static function tostring(L:cpp.RawPointer<Lua_State>, i:Int):String;
 
 	@:native('lua_close')
 	static function close(L:cpp.RawPointer<Lua_State>):Void;
@@ -104,8 +100,13 @@ extern class Lua {
 		return _isstring(L, idx) != 0;
 	}
 
+	@:noCompletion
 	@:native('lua_toboolean')
-	static function toboolean(L:cpp.RawPointer<Lua_State>, idx:Int):Int;
+	static function _toboolean(L:cpp.RawPointer<Lua_State>, idx:Int):Int;
+
+	static inline function toboolean(L:cpp.RawPointer<Lua_State>, idx:Int):Bool {
+		return _toboolean(L, idx) != 0;
+	}
 
 	@:noCompletion
 	@:native('lua_isnumber')
@@ -130,7 +131,7 @@ extern class Lua {
 	static function pcall(L:cpp.RawPointer<Lua_State>, nargs:Int, nresults:Int, errfunc:Int):Int;
 
 	@:native('lua_tointeger')
-	static function tointeger(L:cpp.RawPointer<Lua_State>, idx:Int):Lua_Integer;
+	static function tointeger(L:cpp.RawPointer<Lua_State>, idx:Int):Int;
 
 	@:native('lua_next')
 	static function next(L:cpp.RawPointer<Lua_State>, idx:Int):Int;
